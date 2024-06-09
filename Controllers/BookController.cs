7150dae73806ab903586206c0d868fa5f1c5f103
Seth <sethlwg1@gmail.com>
2024-06-09 17:27:23 -0500

    Added SQLite database and seeded database with a list of books

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -21,7 +21,7 @@ namespace LibraryApp1.Controllers
             return await _context.Books.ToListAsync();
         }
 
-        //Get: api/Books/1
+        //Get: api/details/1
 
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<Book>>> GetBookById(int id)
@@ -36,6 +36,55 @@ namespace LibraryApp1.Controllers
             return Ok(book);
         }
 
+        // PUT: api/details/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try 
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else 
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/details/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
     
     }
 }
--- LibraryDbContext.cs
+++ LibraryDbContext.cs
@@ -2,7 +2,28 @@ using Microsoft.EntityFrameworkCore;
 
 public class LibraryDbContext : DbContext
 {
-    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+    protected readonly IConfiguration Configuration;
 
+    public LibraryDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity => { entity.Property(e => e.Id).IsRequired();});
+
+
+        modelBuilder.Entity<Book>().HasData(
+            new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925, CopyrightInfo = "© 1925 by F. Scott Fitzgerald" },
+             new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960, CopyrightInfo = "© 1960 by Harper Lee" },
+            new Book { Id = 3, Title = "1984", Author = "George Orwell", Year = 1949, CopyrightInfo = "© 1949 by George Orwell" },
+            new Book { Id = 4, Title = "Moby Dick", Author = "Herman Melville", Year = 1851, CopyrightInfo = "© 1851 by Herman Melville" });
+
+    }
     public DbSet<Book> Books {get; set;}
 }
